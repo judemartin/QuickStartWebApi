4375cf21922d101a7ae4124c124b0905d98bbde3
Jude Martin <jude.martin@mic-software.net>
2018-06-07 22:45:16 -0700

    WebApi Starter

---
+++ Controllers/TodoApiController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuickStartWebApi.Data;
+using QuickStartWebApi.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuickStartWebApi.Controllers
+{
+    [Route("api/todos")]
+    public class TodoApiController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public TodoApiController(TodoContext context)
+        {
+            _context = context;
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItemModel { Name = "Item1" });
+                _context.SaveChanges();
+
+            }
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var models = _context.TodoItems.ToList();
+            return Ok(models);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var model = _context.TodoItems.Find(id);
+            if (model == null)
+                return NotFound();
+
+            return Ok(model);
+        }
+    }
+}
---
+++ Data/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using QuickStartWebApi.Models;
+
+namespace QuickStartWebApi.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItemModel> TodoItems { get; set; }
+    }
+}
